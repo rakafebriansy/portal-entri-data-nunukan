6c1db4149b80d2a968700a6cc50e219d3b01b808
rakafebriansy <rakafebrian19@gmail.com>
2025-06-09 16:35:42 +0700

    feat: dynamic navigation from db

--- app/Providers/Filament/EntriDataPanelProvider.php
+++ app/Providers/Filament/EntriDataPanelProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers\Filament;
 
+use App\Models\KategoriSp;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -16,12 +19,29 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Str;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class EntriDataPanelProvider extends PanelProvider
 {
+    private function formatString($string)
+    {
+        $replaced = str_replace(' ', '', strtolower(trim($string)));
+
+        return $replaced;
+    }
+
     public function panel(Panel $panel): Panel
     {
+        $kategoris = KategoriSp::orderBy('nama')->get();
+        $navigations = $kategoris->map(
+            fn($item) =>
+            NavigationItem::make($item->nama)
+                ->url("/entri-data/sp/{$item->bidang}/" . $this->formatString($item->nama))
+                ->icon('heroicon-o-link')
+                ->group(Str::title($item->bidang))
+        )->toArray();
+
         return $panel
             ->id('entri-data')
             ->brandName('Entri Data Penta')
@@ -29,6 +49,7 @@ class EntriDataPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->navigationItems($navigations)
             ->discoverResources(in: app_path('Filament/EntriData/Resources'), for: 'App\\Filament\\EntriData\\Resources')
             ->discoverPages(in: app_path('Filament/EntriData/Pages'), for: 'App\\Filament\\EntriData\\Pages')
             ->pages([
